82c76ff3f0c6b01ee27f3004c5915cd6ac271b72
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2017-08-22 14:23:08 -0400

    Added tests for caverage

--- npstreams/cuda.py
+++ npstreams/cuda.py
@@ -17,7 +17,7 @@ try:
     import pycuda.gpuarray as gpuarray
     import pycuda.autoinit
 except ImportError:
-    raise RuntimeError('PyCUDA is not installed. CUDA capabilities are not available.')
+    raise ImportError('PyCUDA is not installed. CUDA capabilities are not available.')
 
 import pycuda.driver as driver
 from pycuda.compiler import SourceModule
@@ -25,10 +25,10 @@ from pycuda.compiler import SourceModule
 try:
     SourceModule('')
 except driver.CompileError:
-    raise RuntimeError('CUDA compiler not availble.')
+    raise ImportError('CUDA compiler not availble.')
 
 if driver.Device.count() == 0:
-    raise RuntimeError('No GPU is available.')
+    raise ImportError('No GPU is available.')
 
 @array_stream
 def csum(arrays, ignore_nan = False):
@@ -56,3 +56,42 @@ def csum(arrays, ignore_nan = False):
         arr_gpu += gpuarray.to_gpu(arr)
 
     return arr_gpu.get()
+
+@array_stream
+def caverage(arrays, weights, ignore_nan = False):
+    """
+    Average of stream of arrays, possibly weighted.
+
+    Parameters
+    ----------
+    arrays : iterable of ndarrays
+        Arrays to be averaged. This iterable can also a generator.
+    weights : iterable of ndarray, iterable of floats, or None, optional
+        Iterable of weights associated with the values in each item of `images`. 
+        Each value in an element of `images` contributes to the average 
+        according to its associated weight. The weights array can either be a float
+        or an array of the same shape as any element of `images`. If weights=None, 
+        then all data in each element of `images` are assumed to have a weight equal to one.
+    ignore_nan : bool, optional
+        If True, NaNs are set to zero weight. Default is propagation of NaNs.
+    
+    Returns
+    -------
+    cuda_avg : ndarray
+    """
+    first, arrays = peek(arrays)
+    
+    # We make sure that weights is always an array
+    # This simplifies the handling of NaNs.
+    if weights is None:
+        weights = repeat(1)
+    weights = map(partial(np.broadcast_to, shape = first.shape), weights)
+
+    # Need to know which array has NaNs, and modify the weights stream accordingly
+    if ignore_nan:
+        arrays, arrays2 = tee(arrays)
+        weights = map(lambda arr, wgt: np.logical_not(np.isnan(arr)) * wgt, arrays2, weights)
+        arrays = map(np.nan_to_num, arrays)
+    
+    return csum(arrays)/csum(weights)
+    
--- npstreams/tests/test_cuda.py
+++ npstreams/tests/test_cuda.py
@@ -5,17 +5,25 @@ import numpy as np
 import unittest
 
 try:
-    from ..cuda import csum
+    from ..cuda import csum, caverage
     WITH_CUDA = True
-except RuntimeError:
+except ImportError:
     WITH_CUDA = False
 
-@unittest.skipIf(not WITH_CUDA, 'pycuda is not installed/available')
+@unittest.skipIf(not WITH_CUDA, 'PyCUDA is not installed/available')
 class TestCSum(unittest.TestCase):
 
     def test_zero_sum(self):
         stream = repeat(np.zeros( (64,64), dtype = np.float ), times = 5)
         s = csum(stream)
+        self.assertTrue(np.allclose(s, np.zeros((64,64))))
+
+@unittest.skipIf(not WITH_CUDA, 'PyCUDA is not installed/available')
+class TestCAverage(unittest.TestCase):
+
+    def test_zero_avg_no_weights(self):
+        stream = repeat(np.zeros( (64,64), dtype = np.float ), times = 5)
+        s = caverage(stream)
         self.assertTrue(np.allclose(s, np.zeros((64,64))))
 
 if __name__ == '__main__':
